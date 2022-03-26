6447a11cc52b7e0e8f7aad2bed3c7e5c689c8bbe
js <grawires@gmail.com>
2022-03-26 09:28:49 +0100

    json object mapper working good

--- williroi/src/main/java/pl/sikor/williroi/controller/UserController.java
+++ williroi/src/main/java/pl/sikor/williroi/controller/UserController.java
@@ -1,38 +1,39 @@
 package pl.sikor.williroi.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import pl.sikor.williroi.model.account.AccountModel;
-import pl.sikor.williroi.model.account.hotspot.Hotspot;
 
 @RestController
 public class UserController {
 
     @GetMapping("/")
-    public void staradupa(){
+    public void staradupa() {
         String uri = "https://api.helium.io/v1/accounts/12zQHwN4HkZX1f7Noznkc759rfFDkNefMR8gek9MTd8j4y7ftX9/";
 
         RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
 
-        AccountModel account = restTemplate.getForObject(uri, AccountModel.class);
-        
-        AccountModel[] forNow = restTemplate.getForObject(uri, AccountModel[].class);
-        //account = Arrays.asList(forNow);
-        
+        String rawJson = restTemplate.getForObject(uri, String.class);
+        AccountModel account = new AccountModel();
 
+        try {
+            account = mapper.readValue(rawJson, AccountModel.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("https://api.helium.io/v1/hotspots/112WxrNMPwkFBhHTSQmkqMe1VKoCd3zzPVNU5DFziyAjBf61iyfH/rewards/sum?min_time=-120%20day&bucket=day");
-        System.out.println(account.hotspot_count);
+        System.out.println("ACCOUNT DETAILS: ");
         System.out.println(account.balance);
+        System.out.println();
     }
 
-    
 }
--- williroi/src/main/java/pl/sikor/williroi/model/account/AccountModel.java
+++ williroi/src/main/java/pl/sikor/williroi/model/account/AccountModel.java
@@ -1,5 +1,12 @@
 package pl.sikor.williroi.model.account;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@JsonRootName(value = "data")
 public class AccountModel {
     public int validator_count;
     public int staked_balance;
@@ -12,6 +19,9 @@ public class AccountModel {
     public int dc_nonce;
     public int dc_balance;
     public int block;
+    @JsonProperty("balance")
     public int balance;
     public String address;
+
+    ObjectMapper aa = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false);
 }
