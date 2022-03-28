ce838fbd0dc2ca4ce419f5793617122f2f09dd95
grawi-sikorsky <grawires@gmail.com>
2022-03-28 17:23:03 +0200

    docker poprawki id

--- williroi/src/main/java/pl/sikor/williroi/model/User.java
+++ williroi/src/main/java/pl/sikor/williroi/model/User.java
@@ -3,12 +3,19 @@ package pl.sikor.williroi.model;
 import java.util.ArrayList;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import pl.sikor.williroi.model.heliumAPI.account.AccountModel;
 import pl.sikor.williroi.model.heliumAPI.account.hotspot.Hotspot;
 
 @Entity
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String username;
     String password;
     String apiAccountHash;
